096d60dfb948128aa2c00ec1692110d63e492ce9
Orión Alioth Melgarejo Sánchez <orionmelgarejo@gmail.com>
2018-10-12 23:24:53 -0400

    Vistas de Admin 'Añadir Imagen' y 'Añadir Cuenta' añadidas. Todo el CSS casi completo, las faltas más notables están en la vistas de listado de imágenes y cuentas.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'my_profile' => "my_profile#index"
   get 'my_profile/settings' => "my_profile#settings"
   get 'my_profile/images' => "my_profile#images"
+  get 'my_profile/new_image' => "my_profile#new_image"
   get 'my_profile/accounts' => "my_profile#accounts"
+  get 'my_profile/new_account' => "my_profile#new_account"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
