112f7d5ee773e2a2ba0ed7232812a82f4e73129f
jesuspinto <jesusapd4@gmail.com>
2018-10-14 00:35:17 -0400

    actualizacion de las rutas

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions'}
-  resources :profiles
-  get 'my_profile' => "my_profile#index"
+  get 'my_profile/:id' => "my_profile#edit", as: 'profile'
   get 'my_profile/settings' => "my_profile#settings"
   get 'my_profile/images' => "my_profile#images"
   get 'my_profile/new_image' => "my_profile#new_image"
   get 'my_profile/accounts' => "my_profile#accounts"
   get 'my_profile/new_account' => "my_profile#new_account"
+  patch 'my_profile/:id' => "my_profile#update"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
