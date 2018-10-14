eda934372fc5de50f551af0c709d89df345e086b
jesuspinto <jesusapd4@gmail.com>
2018-10-14 02:15:19 -0400

    actuallizaciones de los formularios

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions'}
   get 'my_profile/:id' => "my_profile#edit", as: 'profile'
-  get 'my_profile/settings' => "my_profile#settings"
+  get 'my_profile/settings/:id' => "my_profile#settings", as: 'my_profile_settings'
   get 'my_profile/images' => "my_profile#images"
   get 'my_profile/new_image' => "my_profile#new_image"
   get 'my_profile/accounts' => "my_profile#accounts"
   get 'my_profile/new_account' => "my_profile#new_account"
   patch 'my_profile/:id' => "my_profile#update"
+  patch 'my_profile/settings/:id' => "my_profile#update"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
