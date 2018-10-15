0c52c372fb5fa9552d307430d0c8e2389a4bb54b
jesuspinto <jesusapd4@gmail.com>
2018-10-14 23:21:01 -0400

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'my_profile/:id/images' => "my_profile#images", as: 'my_profile_images'
   get 'my_profile/:id/new_image' => "my_profile#new_image", as: 'my_profile_new_image'
   get 'my_profile/:id/accounts' => "my_profile#accounts", as: 'my_profile_accounts'
-  get 'my_profile/:id/new_account' => "my_profile#new_account", as: 'my_profile_new_account'
+  get 'my_profile/:id/new_account' => "account#new", as: 'my_profile_new_account'
   patch 'my_profile/:id' => "my_profile#update"
   patch 'my_profile/settings/:id' => "my_profile#update"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
