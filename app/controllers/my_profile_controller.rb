db4a29c6192fec37db2621cee80bcbfbba96a6a6
jesuspinto <jesusapd4@gmail.com>
2018-10-16 17:42:56 -0400

    fixes

--- app/controllers/my_profile_controller.rb
+++ app/controllers/my_profile_controller.rb
@@ -18,7 +18,7 @@ class MyProfileController < ApplicationController
   
   def update
     if @my_profile.update(my_profile_params)
-      redirect_to profile_path(current_user), notice: 'Profile was successfully updated.' 
+      redirect_to my_profile_settings_path(current_user), notice: 'Perfil actualizado satisfactoriamente!' 
     end
   end
   
