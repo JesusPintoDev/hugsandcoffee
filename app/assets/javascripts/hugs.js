707a4bf9b7b4021022dcd75f9610f35b80c4a1bf
jesuspinto <jesusapd4@gmail.com>
2018-10-17 15:52:40 -0400

    View Hug whit bugs

--- app/assets/javascripts/hugs.js
+++ app/assets/javascripts/hugs.js
@@ -2,18 +2,23 @@
 document.addEventListener('DOMContentLoaded',() => {
     elem = document.getElementById("myBar"); 
     elem2 = document.getElementById("myProgress");
+    elem3 = document.getElementById("hugMessage");
     limit = elem2.clientWidth;
     padding = limit;
 })
 
 let elem;   
 let elem2;   
+let elem3;
 var height = 0;
 var id = setInterval(frame, 100);
 var padding;
 var pressed = false;
 var limit;
 var number;
+
+visibility: hidden;
+visibility: visible;
 
 
 function frame() {
@@ -27,7 +32,7 @@ function frame() {
         padding = padding -1;
 
         number = ((height / limit) * 30).toString().split('.')
-        elem.innerHTML = number[0] + '.' + number[1].substring(0,2)  + ' seg';
+        elem3.innerHTML = 'Abrazo de ' + number[0] + '.' + number[1].substring(0,2)  + ' segundos';
     }
 }
 
