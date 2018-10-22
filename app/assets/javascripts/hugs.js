e7a81e0c062dc011ff1d8ea23069fbddcdced106
Orión Alioth Melgarejo Sánchez <orionmelgarejo@gmail.com>
2018-10-22 16:21:15 -0400

    Javascript para Cafés

--- app/assets/javascripts/hugs.js
+++ app/assets/javascripts/hugs.js
@@ -2,12 +2,30 @@
 document.addEventListener('DOMContentLoaded',() => {
     elem = document.getElementById("myBar"); 
     elem2 = document.getElementById("myProgress");
+    elem3 = document.getElementById("hugMessage");
+    elem4 = document.getElementById("seconds");
+    elem5 = document.getElementById("hugMessage2");
+    elem6 = document.getElementById("form1");
+    elem7 = document.getElementById("form2");
+    coffeePrice = document.getElementById("coffee_price");
+    questions = document.getElementsByTagName("input");
+    quantityCoffee  = document.getElementById("quantityCoffee");
+    quantityCoffee2  = document.getElementById("quantityCoffee2");
+    coffees = document.getElementById("coffees");
+    
+    
     limit = elem2.clientWidth;
     padding = limit;
 })
 
 let elem;   
 let elem2;   
+let elem3;
+let elem4;
+let elem5;
+let elem6;
+let elem7;
+
 var height = 0;
 var id = setInterval(frame, 100);
 var padding;
@@ -15,6 +33,42 @@ var pressed = false;
 var limit;
 var number;
 
+
+
+
+let questions;
+let coffeePrice;
+let quantityCoffee;
+let quantityCoffee2;
+let coffees;
+
+function multiplicar() {
+    var total = 0;
+    for(var i=0;i < questions.length; i++) {
+        if (questions[i].type == "radio" && questions[i].checked) total += Number(questions[i].value)*Number(coffeePrice.value);
+    }
+    console.log(total);
+    quantityCoffee.innerHTML = 'Total: ' + total.toFixed(2);
+    quantityCoffee2.innerHTML = 'Total: ' + total;
+    coffees.value = total/Number(coffeePrice.value)
+}
+
+
+
+
+
+
+function ocultar() {
+    console.log(elem4)
+    elem6.hidden = true;;
+    elem7.hidden = false;
+}
+
+function regresar() {
+    console.log(elem4)
+    elem7.hidden = true;
+    elem6.hidden = false;
+}
 
 function frame() {
     if ( pressed == true) {
@@ -27,7 +81,9 @@ function frame() {
         padding = padding -1;
 
         number = ((height / limit) * 30).toString().split('.')
-        elem.innerHTML = number[0] + '.' + number[1].substring(0,2)  + ' seg';
+        elem3.innerHTML = 'Abrazo de ' + number[0] + '.' + number[1].substring(0,2)  + ' segundos';
+        elem4.value = number[0] + '.' + number[1].substring(0,2);
+        elem5.innerHTML = 'Enviando un abrazo de ' + number[0] + '.' + number[1].substring(0,2)  + ' segundos';
     }
 }
 
