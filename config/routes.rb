64567a28f531c984d65c3a5fba054d25a2974cbe
jesuspinto <jesusapd4@gmail.com>
2018-10-23 18:20:08 -0400

    modificaciones a la vista de cafes

--- config/routes.rb
+++ config/routes.rb
@@ -27,15 +27,15 @@ Rails.application.routes.draw do
   get 'profiles/:id/reward' => "profiles#reward", as: 'profiles_reward'
   get 'profiles/:id/coffee' => "profiles#coffee", as: 'profiles_coffee'
   get 'profiles/:id/new_hug' => "hugs#new", as: 'new_hug'
-  post 'profiles/:id/hug' => "hugs#create", as: 'testimonials'
-  get 'profiles/:id/hug' => "profiles#hug"
-  get 'profiles/:id/coffe' => "coffes#new"
+  post 'profiles/:id/hug' => "hugs#create"
+  get 'profiles/:id/hug' => "profiles#hug", as: 'testimonials'
+  get 'profiles/:id/coffe' => "coffes#new", as: 'new_coffee'
   get 'profiles/:id/thanks' => "profiles#thanks", as: 'profiles_thanks'
 
 
   get 'profiles/:id' => "profiles#show", as: "profiles"
 
-  post "profiles/:id/coffee" => "profiles#create", as: "testimonials"
+  post "profiles/:id/coffee" => "profiles#create"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
